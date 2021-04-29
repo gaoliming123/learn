75cf93357b71e61f64d11b9b08d64fd9e2b88d5d
gaoliming <xp01255410@icloud.com>
2021-04-29 20:54:11 +0800

    Update hingeloss.py

--- hingeloss/hingeloss.py
+++ hingeloss/hingeloss.py
@@ -24,16 +24,6 @@ class SmoothedHingeLoss(nn.Module):
        loss = (x1.sum() + x2.sum() + x3.sum()) / (len(x1) + len(x2) + len(x3))
        return loss
 
-class OrginHingeLoss(nn.Module):
-     def __init__(self, device):
-         self.device = device
-         super(OrginHingeLoss, self).__init__()
- 
-     def forward(self, pos, neg):
-         y = torch.cat([torch.ones(*pos.shape).to(self.device), - torch.ones(*neg.shape).to(self.device)], dim=0)
-         score = torch.cat([pos, neg], dim=0)
-         loss = torch.clamp(torch.tensor(1.).to(self.device) - y * score, min=0).mean()
-         return loss
 
 class HingeMarginLoss(nn.Module):
     def __init__(self, device, margin):
